78865f2df1cc02beb2b776608995804ea87a5f14
dvsumanth07 <dongavenkatasumanth.20.it@anits.edu.in>
2023-10-23 11:11:45 +0530

    Final 4

--- myapp/src/components/Authentication/Login.js
+++ myapp/src/components/Authentication/Login.js
@@ -51,7 +51,7 @@ export default function Login() {
         isClosable: true,
         position: "bottom",
       });
-      setUser(data);
+      // setUser(data);
       localStorage.setItem("userInfo", JSON.stringify(data));
       setLoading(false);
       history.push("/chats");
